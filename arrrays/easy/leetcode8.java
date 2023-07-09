5f85f419c9a551a7cb96c177d8e522328d25f0f7
Manish <manishdwivedi45165@gmail.com>
2023-07-10 00:06:46 +0530

    cell with odd value in the materix

---
+++ arrrays/easy/leetcode8.java
@@ -0,0 +1,60 @@
+package arrrays.easy;
+
+/*
+ * There is an m x n matrix that is initialized to all 0's. There is also a 2D array indices where each indices[i] = [ri, ci] represents a 0-indexed location to perform some increment operations on the matrix.
+
+For each location indices[i], do both of the following:
+
+Increment all the cells on row ri.
+Increment all the cells on column ci.
+Given m, n, and indices, return the number of odd-valued cells in the matrix after applying the increment to all locations in indices.
+
+ 
+
+Example 1:
+
+
+Input: m = 2, n = 3, indices = [[0,1],[1,1]]
+Output: 6
+Explanation: Initial matrix = [[0,0,0],[0,0,0]].
+After applying first increment it becomes [[1,2,1],[0,1,0]].
+The final matrix is [[1,3,1],[1,3,1]], which contains 6 odd numbers.
+Example 2:
+
+
+Input: m = 2, n = 2, indices = [[1,1],[0,0]]
+Output: 0
+Explanation: Final matrix = [[2,2],[2,2]]. There are no odd numbers in the final matrix.
+ */
+
+public class leetcode8 {
+    
+        public int oddCells(int m, int n, int[][] indices) {
+            int[][] arr = new int[m][n];
+            for(int i = 0;i<indices.length;i++){
+                int temp1=0;
+                int temp2 = 0;
+                for(int j = 0;j<2;j++){
+                         temp1 = indices[i][0];
+                         temp2 = indices[i][1];
+                }
+                for(int j = 0;j<n;j++){
+                    arr[temp1][j]+=1;
+                   
+                }
+                for(int k = 0;k<m;k++){
+                     arr[k][temp2]+=1;
+                }
+            }
+            int cnt =0;
+            for(int i = 0;i<m;i++){
+                for(int j = 0;j<n;j++){
+                   int check = arr[i][j];
+                   if(check%2!=0)cnt++;
+                }
+            }
+            return cnt;
+        
+    }
+    
+}
