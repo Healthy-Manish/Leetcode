0a638da3407c78d98d13340b631295ef24b6f618
Manish <manishdwivedi45165@gmail.com>
2023-07-13 23:41:49 +0530

    two sum problem

--- arrrays/easy/leetcode10.java
+++ arrrays/easy/leetcode10.java
@@ -20,26 +20,50 @@ public class leetcode10 {
     Explanation: 1200 + 34 = 1234
      */
     public static void main(String[] args) {
-     
-        
 
+        StringBuilder str = new StringBuilder("100");
+        String st = str.toString();
+        System.out.println(st);
+        int num = Integer.parseInt(st);
+        System.out.println(num);
+        System.out.println(st.getClass());
+        StringBuilder str2 = new StringBuilder("");
+
+        int[] num2 = {1, 2, 0, 0};
+        int k = 34;
+//        for (int i = 0; i < num2.length; i++) {
+//            str2.append(String.valueOf(num2[i]));
+//        }
+//        int num3 = Integer.parseInt(str2.toString());
+//        int k = 34;
+//        System.out.println(num3+k);
+//        int num4 = num3+k;
+//        List<Integer> list = new ArrayList<>();
+//        while (num4>0){
+//            int temp = num4%10;
+//            list.add(0,temp);
+//            num4=num4/10;
+//
+//        }
+//        System.out.println(list);
+        System.out.println(addToArrayForm(num2, k));
     }
-    class Solution {
-        public List<Integer> addToArrayForm(int[] num, int k) {
-            
-            List<Integer> list = new ArrayList<>();
-    
-            for(int i=num.length-1; i>=0; i--){
-                list.add(0, (num[i]+k)%10); 
-                k = (num[i]+k)/10; 
-            }
-    
-            //add remaining digits of k is any
-            while(k>0){
-                list.add(0, k%10);
-                k = k/10;
-            }
-            return list;
+
+
+    static public List<Integer> addToArrayForm(int[] num, int k) {
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = num.length - 1; i >= 0; i--) {
+            list.add(0, (num[i] + k) % 10);
+            k = (num[i] + k) / 10;
         }
+
+        //add remaining digits of k is any
+        while (k > 0) {
+            list.add(0, k % 10);
+            k = k / 10;
+        }
+        return list;
     }
 }
